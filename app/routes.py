b52894354196002964ba144fcf70eff630358e4f
joselitodasilva <joselito.dasilva32@gmail.com>
2017-04-06 13:53:00 +0100

    including camera stream tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, Response
 from app import app
+from camera import Camera
 
+#FUNCTIONS
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+#ROUTES
 @app.route('/')
 def root():
 	return render_template('root.html')
@@ -8,3 +18,12 @@ def root():
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/cam')
+def cam():
+	return render_template('cam.html')
+
+@app.route('/video_feed')
+def video_feed():
+	return Response(gen(Camera()),
+				mimetype='multipart/x-mixed-replace; boundary=frame')
